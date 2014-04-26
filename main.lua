4d2865ed3b8410ff90df413b3f3750f9752a1e10
Shane Satterfield <dustyplant@gmail.com>
2014-04-25 22:04:58 -0700

    Updated GameObjects.

--- main.lua
+++ main.lua
@@ -2,11 +2,12 @@ require 'src/GameObject'
 
 function love.load()
 	love.physics.setMeter( 64 )
-	world = love.physics.newWorld( 0, 0, true )
+	world = love.physics.newWorld( 0, 9.81 * 64, true )
 	love.window.setMode( 800, 600 )
 	love.graphics.setBackgroundColor( 0, 0, 255 )
 
 	whale = Whale( love.graphics.getWidth() / 2, love.graphics.getHeight() / 2 )
+	dwarf = Dwarves( 0, 0 )
 end
 
 function love.update( dt )
@@ -15,4 +16,5 @@ end
 
 function love.draw()
 	whale:render()
+	dwarf:render()
 end
