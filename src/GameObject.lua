00fa3d29a2d5e31b4b6f01e35e0b6d0978d88736
Shane Satterfield <dustyplant@gmail.com>
2014-04-26 22:19:06 -0700

    Random updates.

--- src/GameObject.lua
+++ src/GameObject.lua
@@ -85,7 +85,7 @@ function Whale:__init( x, y )
 	self.air = 100
 	self.airTicks = 0
 
-	self.body = love.physics.newBody( world, self.pos.x, self.pos.y, "dynamic")
+	self.body = love.physics.newBody( world, self.pos.x, self.pos.y, "dynamic" )
 	self.shape = love.physics.newCircleShape( 100 )
 	self.fixture = love.physics.newFixture( self.body, self.shape, 10 )
 	self.fixture:setUserData( self )
@@ -182,7 +182,6 @@ function Whale:update( dt )
 end
 
 function Whale:render()
-	--love.graphics.circle("fill", self.body:getX(), self.body:getY(), self.shape:getRadius() )
 	if(self.special_state ~= nil) then
 		love.graphics.draw( self.spriteset[self.special_state], self.body:getX() - self:getWidth()/2, self.body:getY() - self:getHeight()/2 )
 		if self.special_state == "hurt" then
@@ -312,11 +311,3 @@ end
 function Ammo:render()
 	love.graphics.draw( self.image, self:getX(), self:getY() )
 end
-
---[[
-Fish = class()
-
-function Fish:__init( x, y )
-
-end
-]]
