98a072227c1f9207119fd672af0bb86237e28350
Shane Satterfield <dustyplant@gmail.com>
2014-04-26 18:08:59 -0700

    Messing around with movement and killing.

--- src/Screen.lua
+++ src/Screen.lua
@@ -106,6 +106,7 @@ function GameScreen:update( dt )
 
 	for i, index in ipairs( removals ) do
 		table.remove( self.objects, index )
+		table.insert( self.objects, Dwarves( love.graphics.getWidth() * math.random() * 3, love.graphics.getHeight() * math.random() * 3 ) )
 	end
 	
 	if posX <= -1 * imageWidth / 2 then posX = 0 end
@@ -127,10 +128,7 @@ end
 function beginContact( a, b, coll )
 	local tempA = a:getUserData()
 	local tempB = b:getUserData()
-	if tempA:is( Whale ) or tempB:is( Whale ) then
-		tempA.toKill = true
-		tempB.toKill = true
-	elseif tempA:is( Shots ) and tempB:is( Dwarves ) or tempA:is( Dwarves ) and tempB:is( Shots ) then
+	if tempA:is( Whale ) or tempB:is( Whale ) or tempA:is( Shots ) and tempB:is( Dwarves ) or tempA:is( Dwarves ) and tempB:is( Shots ) then
 		tempA.toKill = true
 		tempB.toKill = true
 	end
