f3cedab77892b518b9dc3712703afc8a0d61dd2b
Shane Satterfield <dustyplant@gmail.com>
2014-04-26 21:09:24 -0700

    Fixed stuff and the stuff with the ammo.

--- src/Screen.lua
+++ src/Screen.lua
@@ -93,12 +93,12 @@ function GameScreen:__init()
 	world:setCallbacks( beginContact, endContact, preSolve, postSolve )
 
 	self.objects = {}
-	for i = 1, 15 do
+	for i = 1, 1 do
 		spwanDwarf( self.objects )
 	end
 
 	for i = 1, 15 do
-		table.insert( self.objects, Ammo(500, 500) )
+		table.insert( self.objects, Ammo(1000 * math.random(), 1000 * math.random()) )
 	end	
 
 	dims = {}
@@ -152,7 +152,7 @@ function GameScreen:update( dt )
 
 	for i, index in ipairs( removals ) do
 		table.remove( self.objects, index )
-		spwanDwarf( ActiveScreen.objects )
+		--spwanDwarf( ActiveScreen.objects )
 	end
 	
 	if posX <= -1 * imageWidth / 2 then posX = 0 end
